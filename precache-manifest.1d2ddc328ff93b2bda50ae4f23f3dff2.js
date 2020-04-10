4f9a6a57e525d2d0757ffd4dd20363fe14eabdfb
Cameron <csoleimani@gmail.com>
2020-04-10 10:51:45 -0700

    Updates

---
+++ precache-manifest.1d2ddc328ff93b2bda50ae4f23f3dff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606652d02dfdc4f48b8eb5fd3ca31f50",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "871b0a6e01052b939d5c",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eca4e42f4d63c8db707d",
+    "url": "/react-test/static/js/2.a5a4e00c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.a5a4e00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871b0a6e01052b939d5c",
+    "url": "/react-test/static/js/main.99ef6eed.chunk.js"
+  },
+  {
+    "revision": "40fca6ad130dbb0c161c",
+    "url": "/react-test/static/js/runtime-main.93718460.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
